b9e164f369eaef1b584373f26418a03e42fa130e
troushoo <troushoo@gmail.com>
2018-11-23 22:22:00 +0900

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace sqlcommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=localhost;Initial Catalog=testdb;Integrated Security=True;";
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                string sqlquery = "SELECT * FROM mytable";
+
+                SqlCommand command = new SqlCommand(sqlquery, cnn);
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Console.WriteLine(dataReader.GetValue(0).ToString() + " " + dataReader.GetValue(1).ToString() + " " + dataReader.GetValue(2).ToString());
+                }
+
+                dataReader.Close();
+                command.Dispose();
+                System.Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot open connection");
+            }
+
+            cnn.Close();
+
+
+
+        }
+    }
+}
